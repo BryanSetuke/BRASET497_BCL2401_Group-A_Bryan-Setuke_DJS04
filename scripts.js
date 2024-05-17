078974d82b8e665f347970bf57f4910629fa1840
BryanSetuke <156883514+BryanSetuke@users.noreply.github.com>
2024-05-17 19:37:49 +0200

    Commit

--- scripts.js
+++ scripts.js
@@ -3,7 +3,104 @@ import { books, authors, genres, BOOKS_PER_PAGE } from "./data.js";
 
 // Initialize page number and matches array
 let page = 1;
+
+class BookPreview extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    static get observedAttributes() {
+        return ["author", "title", "image"];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.render();
+    }
+
+    render() {
+        const author = this.getAttribute("author");
+        const title = this.getAttribute("title");
+        const image = this.getAttribute("image");
+
+        this.shadowRoot.innerHTML = `
+		<style>
+		.preview {
+		  border-width: 0;
+		  width: 100%;
+		  font-family: Roboto, sans-serif;
+		  padding: 0.5rem 1rem;
+		  display: flex;
+		  align-items: center;
+		  cursor: pointer;
+		  text-align: left;
+		  border-radius: 8px;
+		  border: 1px solid rgba(var(--color-dark), 0.15);
+		  background: rgba(var(--color-light), 1);
+		}
+		
+		@media (min-width: 60rem) {
+		  .preview {
+			padding: 1rem;
+		  }
+		}
+		
+		.preview_hidden {
+		  display: none;
+		}
+		
+		.preview:hover {
+		  background: rgba(var(--color-blue), 0.05);
+		}
+		
+		.preview__image {
+		  width: 48px;
+		  height: 70px;
+		  object-fit: cover;
+		  background: grey;
+		  border-radius: 2px;
+		  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+			0px 1px 1px 0px rgba(0, 0, 0, 0.1), 0px 1px 3px 0px rgba(0, 0, 0, 0.1);
+		}
+		
+		.preview__info {
+		  padding: 1rem;
+		}
+		
+		.preview__title {
+		  margin: 0 0 0.5rem;
+		  font-weight: bold;
+		  display: -webkit-box;
+		  -webkit-line-clamp: 2;
+		  -webkit-box-orient: vertical;  
+		  overflow: hidden;
+		  color: rgba(var(--color-dark), 0.8)
+		}
+		
+		.preview__author {
+		  color: rgba(var(--color-dark), 0.4);
+		}
+            </style>
+            <button class="preview" data-preview="${this.getAttribute("id")}">
+                <img class="preview__image" src="${image}" />
+                <div class="preview__info">
+                    <h3 class="preview__title">${title}</h3>
+                    <div class="preview__author">${author}</div>
+                </div>
+            </button>
+        `;
+    }
+}
+
+customElements.define("book-preview", BookPreview);
+
+
 let matches = books; // Initially, all books are considered matches
+
 
 // Function to get DOM elements
 const getElement = (selector) => document.querySelector(selector);
@@ -26,7 +123,15 @@ const createBookPreviews = (books, container) => {
     });
     container.appendChild(fragment);
 };
+function createBookPreview({ author, id, image, title }) {
+    const element = document.createElement("book-preview");
+    element.setAttribute("id", id);
+    element.setAttribute("author", author);
+    element.setAttribute("title", title);
+    element.setAttribute("image", image);
 
+    return element;
+}
 // Function to create and append options to a select element
 const createOptions = (options, defaultOption, container) => {
     const fragment = document.createDocumentFragment();
